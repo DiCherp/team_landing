f8992ae60c5ec64e128ac80c7fa45bdecd51e827
Dmitriy <d.cherpickij2210@gmail.com>
2025-10-10 17:20:59 +0300

    added cookie and form registration

---
+++ script.js
@@ -0,0 +1,27 @@
+function openRegistr(){
+    document.getElementById("myRegistr").style.display = "block";
+}
+
+function closeRegistr(){
+    document.getElementById("myRegistr").style.display = "none";
+}
+
+document.getElementById('file-upload').addEventListener('change', function(e) {
+        var fileName = e.target.files[0] ? e.target.files[0].name : 'Добавить документ';
+        document.getElementById('file-name').textContent = fileName;
+    });
+    
+
+function closeCookie(){
+    document.getElementById("myCookie").style.display = "none";
+}
+
+document.querySelector('.cookie-accepting').classList.add('show');
+
+function openCookie(){
+    document.getElementById("myCookie").style.display = "flex";
+}
+
+function cookie_acceptingClose(){
+     document.getElementById("cookie-accepting").style.display = "none";
+}
