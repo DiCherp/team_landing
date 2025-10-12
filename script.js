54575e872d50f43bc7539de0b57a5b706136b3ce
Dmitriy <d.cherpickij2210@gmail.com>
2025-10-12 19:32:23 +0300

    fixed adaptive and js

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ function closeRegistr(){
 }
 
 document.getElementById('file-upload').addEventListener('change', function(e) {
-        var fileName = e.target.files[0] ? e.target.files[0].name : 'Добавить документ';
-        document.getElementById('file-name').textContent = fileName;
-    });
-    
+    var fileName = e.target.files[0] ? e.target.files[0].name : 'Добавить документ';
+    document.getElementById('file-name').textContent = fileName;
+});
 
 function closeCookie(){
     document.getElementById("myCookie").style.display = "none";
 }
 
-document.querySelector('.cookie-accepting').classList.add('show');
+let cookie = document.querySelector('.cookie-accepting')
+if (!!cookie){
+    cookie.classList.add('show');
+}
 
 function openCookie(){
     document.getElementById("myCookie").style.display = "flex";
